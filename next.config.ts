308768b162182777126dfe8561f752317154abf4
Modh Harshil Chetankumar <78226025+HarshilModh@users.noreply.github.com>
2025-11-22 15:43:28 -0500

    vapi api created

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig: NextConfig & { eslint?: { ignoreDuringBuilds?: boolean } } = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   /* config options here */
   reactCompiler: true,
 };
